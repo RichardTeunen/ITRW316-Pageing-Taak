2c43c04db656f586135d1815a523c1a351bcd3b2
Christiaan Koch <christiaankoch66@gmail.com>
2019-04-08 19:36:12 +0200

    Cleaned up the GUI.

--- Paging/Paging/Form1.Designer.cs
+++ Paging/Paging/Form1.Designer.cs
@@ -150,7 +150,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Paging with TLB";
             this.ResumeLayout(false);
             this.PerformLayout();
 
