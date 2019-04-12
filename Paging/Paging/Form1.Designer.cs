48ea22713b83b0badc9955c82edadf832c4c58ac
Christiaan Koch <christiaankoch66@gmail.com>
2019-04-12 14:31:14 +0200

    Add process finished

--- Paging/Paging/Form1.Designer.cs
+++ Paging/Paging/Form1.Designer.cs
@@ -29,10 +29,7 @@
         private void InitializeComponent()
         {
             this.lstChanges = new System.Windows.Forms.ListBox();
-            this.btnAddPage = new System.Windows.Forms.Button();
-            this.btnClear = new System.Windows.Forms.Button();
-            this.btnGlobal = new System.Windows.Forms.Button();
-            this.btnLocal = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
             this.lsbTLB = new System.Windows.Forms.ListBox();
             this.label3 = new System.Windows.Forms.Label();
             this.lstSecondary = new System.Windows.Forms.ListBox();
@@ -40,67 +37,51 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.btnAddPage = new System.Windows.Forms.Button();
+            this.gbxAddProcess = new System.Windows.Forms.GroupBox();
+            this.nudPageCount = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radGlobal = new System.Windows.Forms.RadioButton();
+            this.radLocal = new System.Windows.Forms.RadioButton();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.gbxAddProcess.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPageCount)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lstChanges
             // 
             this.lstChanges.FormattingEnabled = true;
-            this.lstChanges.Location = new System.Drawing.Point(248, 24);
+            this.lstChanges.Location = new System.Drawing.Point(15, 320);
             this.lstChanges.Name = "lstChanges";
-            this.lstChanges.Size = new System.Drawing.Size(454, 134);
+            this.lstChanges.Size = new System.Drawing.Size(690, 134);
             this.lstChanges.TabIndex = 33;
             this.lstChanges.SelectedIndexChanged += new System.EventHandler(this.lstChanges_SelectedIndexChanged);
             // 
-            // btnAddPage
+            // btnReset
             // 
-            this.btnAddPage.Location = new System.Drawing.Point(12, 24);
-            this.btnAddPage.Name = "btnAddPage";
-            this.btnAddPage.Size = new System.Drawing.Size(214, 30);
-            this.btnAddPage.TabIndex = 32;
-            this.btnAddPage.Text = "Add Page to Secondary Storage";
-            this.btnAddPage.UseVisualStyleBackColor = true;
-            this.btnAddPage.Click += new System.EventHandler(this.btnAddPage_Click);
-            // 
-            // btnClear
-            // 
-            this.btnClear.Location = new System.Drawing.Point(12, 129);
-            this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(214, 28);
-            this.btnClear.TabIndex = 31;
-            this.btnClear.Text = "Reset";
-            this.btnClear.UseVisualStyleBackColor = true;
-            // 
-            // btnGlobal
-            // 
-            this.btnGlobal.Location = new System.Drawing.Point(12, 95);
-            this.btnGlobal.Name = "btnGlobal";
-            this.btnGlobal.Size = new System.Drawing.Size(214, 28);
-            this.btnGlobal.TabIndex = 30;
-            this.btnGlobal.Text = "Perform Global Page Swap";
-            this.btnGlobal.UseVisualStyleBackColor = true;
-            // 
-            // btnLocal
-            // 
-            this.btnLocal.Location = new System.Drawing.Point(12, 60);
-            this.btnLocal.Name = "btnLocal";
-            this.btnLocal.Size = new System.Drawing.Size(214, 29);
-            this.btnLocal.TabIndex = 29;
-            this.btnLocal.Text = "Perform Local Page Swap";
-            this.btnLocal.UseVisualStyleBackColor = true;
+            this.btnReset.Location = new System.Drawing.Point(427, 74);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(150, 28);
+            this.btnReset.TabIndex = 31;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
             // 
             // lsbTLB
             // 
             this.lsbTLB.FormattingEnabled = true;
-            this.lsbTLB.Location = new System.Drawing.Point(488, 189);
+            this.lsbTLB.Location = new System.Drawing.Point(491, 133);
             this.lsbTLB.Name = "lsbTLB";
-            this.lsbTLB.Size = new System.Drawing.Size(214, 238);
+            this.lsbTLB.Size = new System.Drawing.Size(214, 160);
             this.lsbTLB.TabIndex = 28;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.label3.Location = new System.Drawing.Point(487, 173);
+            this.label3.Location = new System.Drawing.Point(490, 117);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(132, 26);
             this.label3.TabIndex = 27;
@@ -109,24 +90,24 @@
             // lstSecondary
             // 
             this.lstSecondary.FormattingEnabled = true;
-            this.lstSecondary.Location = new System.Drawing.Point(248, 189);
+            this.lstSecondary.Location = new System.Drawing.Point(251, 133);
             this.lstSecondary.Name = "lstSecondary";
-            this.lstSecondary.Size = new System.Drawing.Size(214, 238);
+            this.lstSecondary.Size = new System.Drawing.Size(214, 160);
             this.lstSecondary.TabIndex = 26;
             // 
             // lstPhysical
             // 
             this.lstPhysical.FormattingEnabled = true;
-            this.lstPhysical.Location = new System.Drawing.Point(12, 189);
+            this.lstPhysical.Location = new System.Drawing.Point(15, 133);
             this.lstPhysical.Name = "lstPhysical";
-            this.lstPhysical.Size = new System.Drawing.Size(214, 238);
+            this.lstPhysical.Size = new System.Drawing.Size(214, 160);
             this.lstPhysical.TabIndex = 25;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.label2.Location = new System.Drawing.Point(245, 173);
+            this.label2.Location = new System.Drawing.Point(248, 117);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(131, 13);
             this.label2.TabIndex = 24;
@@ -136,7 +117,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.label1.Location = new System.Drawing.Point(9, 173);
+            this.label1.Location = new System.Drawing.Point(12, 117);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(119, 13);
             this.label1.TabIndex = 23;
@@ -146,24 +127,130 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.label4.Location = new System.Drawing.Point(245, 9);
+            this.label4.Location = new System.Drawing.Point(12, 305);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(49, 13);
             this.label4.TabIndex = 34;
             this.label4.Text = "Changes";
+            // 
+            // btnAddPage
+            // 
+            this.btnAddPage.Location = new System.Drawing.Point(22, 60);
+            this.btnAddPage.Name = "btnAddPage";
+            this.btnAddPage.Size = new System.Drawing.Size(85, 21);
+            this.btnAddPage.TabIndex = 32;
+            this.btnAddPage.Text = "Add a process";
+            this.btnAddPage.UseVisualStyleBackColor = true;
+            this.btnAddPage.Click += new System.EventHandler(this.btnAddPage_Click);
+            // 
+            // gbxAddProcess
+            // 
+            this.gbxAddProcess.Controls.Add(this.nudPageCount);
+            this.gbxAddProcess.Controls.Add(this.label5);
+            this.gbxAddProcess.Controls.Add(this.btnAddPage);
+            this.gbxAddProcess.Location = new System.Drawing.Point(127, 9);
+            this.gbxAddProcess.Name = "gbxAddProcess";
+            this.gbxAddProcess.Size = new System.Drawing.Size(137, 93);
+            this.gbxAddProcess.TabIndex = 38;
+            this.gbxAddProcess.TabStop = false;
+            this.gbxAddProcess.Text = "Add Process and Pages";
+            // 
+            // nudPageCount
+            // 
+            this.nudPageCount.Location = new System.Drawing.Point(23, 34);
+            this.nudPageCount.Maximum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.nudPageCount.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudPageCount.Name = "nudPageCount";
+            this.nudPageCount.Size = new System.Drawing.Size(84, 20);
+            this.nudPageCount.TabIndex = 39;
+            this.nudPageCount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 13);
+            this.label5.TabIndex = 38;
+            this.label5.Text = "Number of pages";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radGlobal);
+            this.groupBox1.Controls.Add(this.radLocal);
+            this.groupBox1.Location = new System.Drawing.Point(283, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(108, 92);
+            this.groupBox1.TabIndex = 39;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Swapping Type";
+            // 
+            // radGlobal
+            // 
+            this.radGlobal.AutoSize = true;
+            this.radGlobal.Location = new System.Drawing.Point(25, 56);
+            this.radGlobal.Name = "radGlobal";
+            this.radGlobal.Size = new System.Drawing.Size(55, 17);
+            this.radGlobal.TabIndex = 1;
+            this.radGlobal.TabStop = true;
+            this.radGlobal.Text = "Global";
+            this.radGlobal.UseVisualStyleBackColor = true;
+            // 
+            // radLocal
+            // 
+            this.radLocal.AutoSize = true;
+            this.radLocal.Location = new System.Drawing.Point(25, 33);
+            this.radLocal.Name = "radLocal";
+            this.radLocal.Size = new System.Drawing.Size(51, 17);
+            this.radLocal.TabIndex = 0;
+            this.radLocal.TabStop = true;
+            this.radLocal.Text = "Local";
+            this.radLocal.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(427, 43);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(150, 28);
+            this.btnStop.TabIndex = 40;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(427, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(150, 28);
+            this.btnStart.TabIndex = 41;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(717, 442);
+            this.ClientSize = new System.Drawing.Size(717, 472);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gbxAddProcess);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.lstChanges);
-            this.Controls.Add(this.btnAddPage);
-            this.Controls.Add(this.btnClear);
-            this.Controls.Add(this.btnGlobal);
-            this.Controls.Add(this.btnLocal);
+            this.Controls.Add(this.btnReset);
             this.Controls.Add(this.lsbTLB);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.lstSecondary);
@@ -171,8 +258,13 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Paging with TLB";
+            this.Text = " Paging with TLB";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbxAddProcess.ResumeLayout(false);
+            this.gbxAddProcess.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPageCount)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -181,10 +273,7 @@
         #endregion
 
         private System.Windows.Forms.ListBox lstChanges;
-        private System.Windows.Forms.Button btnAddPage;
-        private System.Windows.Forms.Button btnClear;
-        private System.Windows.Forms.Button btnGlobal;
-        private System.Windows.Forms.Button btnLocal;
+        private System.Windows.Forms.Button btnReset;
         private System.Windows.Forms.ListBox lsbTLB;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ListBox lstSecondary;
@@ -192,6 +281,15 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAddPage;
+        private System.Windows.Forms.GroupBox gbxAddProcess;
+        private System.Windows.Forms.NumericUpDown nudPageCount;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radGlobal;
+        private System.Windows.Forms.RadioButton radLocal;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
     }
 }
 
--- Paging/Paging/Form1.cs
+++ Paging/Paging/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,11 +13,14 @@ namespace Paging
 {
     public partial class Form1 : Form
     {
-        List<int[]> primary = new List<int[]>();
-        List<int[]> secondary = new List<int[]>();
-        List<int[]> tlb = new List<int[]>();
+        List<List<int[]>> processes = new List<List<int[]>>();
+        List<int> primary = new List<int>();
+        List<int> secondary = new List<int>();
+        List<int> tlb = new List<int>();
 
-        int pageCount = 0;
+        int processCount = 0,
+            pageCount = 0,
+            primaryMemorySize = 10;
 
         public Form1()
         {
@@ -30,12 +34,15 @@ namespace Paging
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            for (int x = 1; x <= 10; x++)
+            radLocal.Checked = true;
+            /*
+            for (int x = 1; x <= 3; x++)
             {
-                secondary.Add(new int[] { x, 0 });
+                secondary.Add(new object[] { x, 0 });
                 lstSecondary.Items.Add("Page " + x);
                 pageCount++;
-            }           
+            } 
+            */
         }
 
         private void lstChanges_SelectedIndexChanged(object sender, EventArgs e)
@@ -45,8 +52,62 @@ namespace Paging
 
         private void btnAddPage_Click(object sender, EventArgs e)
         {
-            secondary.Add(new int[] { pageCount++, 0 });
-            lstSecondary.Items.Add("Page " + pageCount);
+            int pagesAdded = Convert.ToInt32(nudPageCount.Value);
+            string letter = "ABCDEFGH"; 
+
+            processes.Add(new List<int[]>());
+
+            for (int x = 0; x < pagesAdded; x++)
+            {
+                processes[processCount].Add(new int[] { pageCount + x, 0 });
+
+                if (primary.Count < primaryMemorySize)
+                {
+                    primary.Add(pageCount + x);
+                    lstPhysical.Items.Add("Page " + (processCount + 1) + letter[x]);
+                }
+                else
+                {
+                    secondary.Add(pageCount + x);
+                    lstSecondary.Items.Add("Page " + (processCount + 1) + letter[x]);
+                }
+            }
+
+            processCount++;
+            pageCount += pagesAdded;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radLocal.Checked)
+            {
+                
+            }
+        }
+
+        public void LocalClockPageReplacement()
+        {
+            /*
+            while (true)
+            {
+                int counter = 0;
+
+                for (int x = 0; x < processes.Count; x++)
+                {
+                    int index = counter % processes[x].Count;
+
+                    //if (primary.Le)
+
+                }
+
+                Thread.Sleep(1000);
+            } 
+            */
+        }
+
+        private void cbxPage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
